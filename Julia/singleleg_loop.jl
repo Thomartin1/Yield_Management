22b5a3429c963adaef2f251393b1d46d7daefb54
Sebastien Descahmps <sebastien.deschamps@eleves.enpc.fr>
2016-04-11 17:22:24 +0200

    Quelques modifs

--- Julia/singleleg_loop.jl
+++ Julia/singleleg_loop.jl
@@ -1,4 +1,4 @@
-include("ComputeBP")# c'est la resolution du probleme dual avec le solveur. 
+include("ComputeBP")# c'est la resolution du probleme dual avec le solveur.
 
 #L'indicage t est pour le temps, il va de 1 à T.
 T= [1:5;] #attetion en Julia on commence à 1
@@ -10,7 +10,6 @@ C=[4,3,5]
 
 
 #Initialisation :
-#Résoudre (LP) -> obtenir la solution du problème dual BP = (BP1 … BPk) directement avec Gurobi.
 bidprice=ComputeBP(x) #le bid-price depend des place restantes. bidprice[1..cl]
 x=C
 #Boucle :
@@ -20,7 +19,6 @@ for it in IT
        for j in cl
        B=bidprice[j] #parcequ'on a qu'un leg sinon il faut sommer les bidprice sur les leg del'it
         if(R[it][t][j]>=B)  #en vrai on devrait avoir une somme si il y a plusieurs legs
-            #On accepte la requête : ditj = ditj – 1 et on résout de nouveau LP pour obtenir la solution du problème dual BP = (BP1 … BPk).
             if(x[j]>0) #pour plusieur leg il faut s'assurer d'avoir la capacité sur tous les leg
                 x[j]=x[j]-1
                 bidprice=ComputeBP(x)
