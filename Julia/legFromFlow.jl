84e7277bedfbffa72cfb39ee0f2b277dd2187ea9
Sebastien Descahmps <sebastien.deschamps@eleves.enpc.fr>
2016-04-22 15:01:01 +0200

    Fonction legFromFlow fonctionelle
    	- elle renvoie une dictionnaire qui à un FlowId renvoie les numéros de vols correspondants
    	- Il faut changer le path du fichier csv correspondant pour que ca marche

--- Julia/legFromFlow.jl
+++ Julia/legFromFlow.jl
@@ -1,11 +1,16 @@
+using DataFrames
+
 # Fonction qui renvoie les vols utilisées par un itinéraire
-function legFromFlow(Flow)
+
+file = readtable("/home/sebastien/Documents/Projet_Air_France/p3.csv")
+
+function legFromFlow()
   FlowLeg = Dict{Int64,Set{UTF8String}}()
-  for i = 1:size(Flow)
-    if in(file[1][i] FlowLeg),collect(values(FlowLeg)))
-      FlowLeg[file[1][i]] = union!(FlowLeg[file[1][i]],file[7][i])
+  for i = 1:size(file[1])[1]
+    if in(file[1][i],keys(FlowLeg))
+      FlowLeg[file[1][i]] = union!(FlowLeg[file[1][i]],[file[7][i]])
     else
-      FlowLeg[file[1][i]] = Set(file[7][i])
+      FlowLeg[file[1][i]] = Set([file[7][i]])
     end
   end
   return FlowLeg
