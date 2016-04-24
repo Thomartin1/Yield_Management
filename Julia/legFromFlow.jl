aaf0e157a60b2a838c16d7595ec20731e46aaaee
Sebastien Descahmps <sebastien.deschamps@eleves.enpc.fr>
2016-04-24 23:46:43 +0200

    Petit probleme de convert avec les chaines ASCII
    Je sais pas trop pourquoi il y a un pb à la ligne 12, si vous trouvez je suis curieux de savoir
    Je fais ca pour avoir acces à la cbine de chaque vol, qui nous sert dan l'algo du bid price

--- Julia/legFromFlow.jl
+++ Julia/legFromFlow.jl
@@ -5,13 +5,15 @@ using DataFrames
 file = readtable("/home/sebastien/Documents/Projet_Air_France/p3.csv")
 
 function legFromFlow()
-  FlowLeg = Dict{Int64,Set{UTF8String}}()
+  FlowLeg = Dict{Int64,Set{ASCIIString}}()
   for i = 1:size(file[1])[1]
     if in(file[1][i],keys(FlowLeg))
-      FlowLeg[file[1][i]] = union!(FlowLeg[file[1][i]],[file[7][i]])
+      FlowLeg[file[1][i]] = union!(FlowLeg[file[1][i]],join([file[7][i],file[9][i]],"-"))
     else
-      FlowLeg[file[1][i]] = Set([file[7][i]])
+      FlowLeg[file[1][i]] = Set([join([file[7][i],file[9][i]],"-")])
     end
   end
   return FlowLeg
 end
+
+print(legFromFlow())
